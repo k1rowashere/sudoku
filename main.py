c4a42aaa70046db26893ac0d903cc5deda6f2cc0
k1rowashere <Kyrollos Youssef@ABOOOKH>
2025-04-28 23:25:19 +0300

    backtracking using ac3 & refactoring

--- main.py
+++ main.py
@@ -3,29 +3,27 @@ from typing import List, Tuple
 
 
 class Sudoku:
-    def __init__(self, initial_grid: List[List[int]] | np.ndarray = None):
-        if isinstance(initial_grid, np.ndarray):
-            self.initial_grid = initial_grid.copy()
-        self.initial_grid = np.array(initial_grid, np.uint8) if initial_grid is not None else None
-        self.grid = np.zeros((9, 9), np.uint8) if initial_grid is None else self.initial_grid.copy()
+    def __init__(self, initial_grid: List[List[int]] | np.ndarray = None, domains: np.ndarray = None):
+        self.initial_grid = (
+            initial_grid if isinstance(initial_grid, np.ndarray) else
+            np.array(initial_grid, np.uint8) if isinstance(initial_grid, list) else
+            None
+        )
+        self.grid: np.ndarray[set[int]]
+        if domains is not None:
+            self.grid = domains
+        else:
+            self.grid = np.ndarray((9, 9), dtype=object)
+            for x in [(i, j) for i in range(9) for j in range(9)]:
+                if self.initial_grid[x] != 0:
+                    self.grid[x] = {int(self.initial_grid[x])}
+                else:
+                    self.grid[x] = {i for i in range(1, 10)}
 
     def __str__(self):
-        string = "╭───────┬───────┬───────╮\n"
-        for i in range(9):
-            if i % 3 == 0 and i != 0:
-                string += "├───────┼───────┼───────┤\n"
-            string += '│ '
-            for j in range(9):
-                string += str(self.grid[i, j]) if self.grid[i, j] != 0 else ' '
-                string += ' '
-                if j % 3 == 2 and j != 8:
-                    string += '│ '
-            string += '│\n'
-        string += "╰───────┴───────┴───────╯"
-        return string
+        return self.__format__(self)
 
-    def color_str(self):
-        # color the initial grid with red
+    def __format__(self, format_spec):
         string = "╭───────┬───────┬───────╮\n"
         for i in range(9):
             if i % 3 == 0 and i != 0:
@@ -33,9 +31,14 @@ class Sudoku:
             string += '│ '
             for j in range(9):
                 if self.initial_grid[i, j] != 0:
-                    string += f"\033[1;31m{self.grid[i, j]}\033[0m"
+                    if format_spec == 'color':
+                        string += f"\033[1;31m{self.initial_grid[i, j]}\033[0m"
+                    else:
+                        string += str(self.initial_grid[i, j])
+                elif len(self.grid[i, j]) == 1:
+                    string += str(list(self.grid[i, j])[0])
                 else:
-                    string += str(self.grid[i, j]) if self.grid[i, j] != 0 else ' '
+                    string += '_'
                 string += ' '
                 if j % 3 == 2 and j != 8:
                     string += '│ '
@@ -44,116 +47,92 @@ class Sudoku:
         string += "╰───────┴───────┴───────╯"
         return string
 
-    def is_valid(self, i, j, num):
-        if num in self.grid[i]:
-            return False
-        if num in self.grid[:, j]:
-            return False
-        box_row, box_col = i // 3, j // 3
-        if num in self.grid[box_row * 3:box_row * 3 + 3, box_col * 3:box_col * 3 + 3]:
-            return False
-        return True
-
-    def check(self):
-        for i in range(9):
-            for j in range(9):
-                if self.grid[i, j] == 0:
-                    continue
-                # remove the number from the grid
-                num = self.grid[i, j]
-                self.grid[i, j] = 0
-                is_valid = self.is_valid(i, j, num)
-                self.grid[i, j] = num
-                if not is_valid:
-                    return False
-        return True
-
     def solve(self):
-        if self.check():
-            return self._do_solve()
-        else:
-            return False
+        self.grid = self.ac3(self.grid)
 
-    def has_solution(self):
-        test_game = Sudoku(self.grid)
-        return test_game.solve()
-
-    def _do_solve(self):
+        # TODO:
+        #  - heuristics (MRV, LCV)
+        #  - handle multiple solutions
         for i in range(9):
             for j in range(9):
-                if self.grid[i, j] != 0:
+                cell = (i, j)
+                if len(self.grid[cell]) == 1:
                     continue
-                for num in range(1, 10):
-                    if not self.is_valid(i, j, num):
-                        continue
+                if len(self.grid[cell]) == 0:
+                    return False
 
-                    self.grid[i, j] = num
-                    if self._do_solve():
-                        return True
-                    self.grid[i, j] = 0
-                return False
-        return True
+                to_remove = []
 
+                # if the cell has more than one possibility, try each one
+                # use ac3 to reduce the search space
+                for num in self.grid[cell]:
+                    new_grid = self.grid.copy()
+                    new_grid[cell] = {num}
 
-def ac3(sudoku):
-    """
-    AC-3 algorithm for solving Sudoku
-    https://en.wikipedia.org/wiki/AC-3_algorithm
+                    new_sudoku = Sudoku(self.initial_grid, new_grid)
 
+                    new_sudoku = new_sudoku.solve()
+                    if new_sudoku is False:
+                        to_remove.append(num)
+                    else:
+                        return new_sudoku
 
-    :param sudoku:
-    :return:
-    """
+                # remove the numbers that didn't work
+                for num in to_remove:
+                    self.grid[cell].remove(num)
+        return self
 
-    X = [(i, j) for i in range(9) for j in range(9)]
-
-    domains = np.array([[set()] * 9 for _ in range(9)], dtype=object)
-    for x in X:
-        if sudoku.grid[x] != 0:
-            domains[x] = {int(sudoku.grid[x])}
-        else:
-            domains[x] = {i for i in range(1, 10)}
-
-    def r2(x: Tuple[int, int], y: Tuple[int, int]):
-        return (x[0] != y[0] or x[1] != y[1]) and (
-                (x[0] == y[0]) or  # same row
-                (x[1] == y[1]) or  # same column
-                (x[0] // 3 == y[0] // 3 and x[1] // 3 == y[1] // 3)  # same box
-        )
-
-    def arc_reduce(x, y):
+    @staticmethod
+    def ac3(domains):
         """
-        Arc reduce the domain of x with respect to y
+        AC-3 algorithm for arc consistency
+        https://en.wikipedia.org/wiki/AC-3_algorithm
 
-        :param x: variable x
-        :param y: variable y
+        :param domains: domains of the variables
         :return:
         """
 
-        change = False
-        to_remove = []
+        def r2(x: Tuple[int, int], y: Tuple[int, int]):
+            return (x[0] != y[0] or x[1] != y[1]) and (
+                    (x[0] == y[0]) or  # same row
+                    (x[1] == y[1]) or  # same column
+                    (x[0] // 3 == y[0] // 3 and x[1] // 3 == y[1] // 3)  # same box
+            )
 
-        for vx in domains[x]:
-            if not any(vx != vy for vy in domains[y]):  # TODO: add a generic constraint check
-                to_remove.append(vx)
-                change = True
+        def arc_reduce(x, y):
+            """
+            Arc reduce the domain of x with respect to y
 
-        for vx in to_remove:
-            domains[x].remove(vx)
+            :param x: variable x
+            :param y: variable y
+            :return:
+            """
 
-        return change
+            change = False
+            to_remove = []
 
-    worklist = [(x, y) for x in X for y in X if r2(x, y)]
+            for vx in domains[x]:
+                if not any(vx != vy for vy in domains[y]):  # TODO: add a generic constraint check
+                    to_remove.append(vx)
+                    change = True
 
-    while worklist:
-        x, y = worklist.pop()
-        if arc_reduce(x, y):
-            if not domains[x]:
-                return False
-            for z in X:
-                if z != y and r2(z, x):
-                    worklist.append((z, x))
-    return domains
+            for vx in to_remove:
+                domains[x].remove(vx)
+
+            return change
+
+        X = [(i, j) for i in range(9) for j in range(9)]
+        worklist = [(x, y) for x in X for y in X if r2(x, y)]
+
+        while worklist:
+            x, y = worklist.pop()
+            if arc_reduce(x, y):
+                if not domains[x]:
+                    return False
+                for z in X:
+                    if z != y and r2(z, x):
+                        worklist.append((z, x))
+        return domains
 
 
 def main():
@@ -170,12 +149,7 @@ def main():
     ]
 
     sudoku = Sudoku(sudoku)
-    print(sudoku.color_str())
-
-    print(ac3(sudoku))
-
-    sudoku.solve()
-    print(sudoku.color_str())
+    print(f'{sudoku.solve():color}')
 
 
 if __name__ == '__main__':
