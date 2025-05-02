fa1cf0a25f158153a0d18269cbe101a8f7aef29f
k1rowashere <Kyrollos Youssef@ABOOOKH>
2025-05-03 02:59:06 +0300

    🚀

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 from enum import Enum
 import numpy as np
 from typing import List, Tuple
@@ -57,43 +58,41 @@ class Sudoku:
 
     def solve(self) -> SolutionState:
         # use ac3 to reduce the search space
-        if self.ac3(self.grid) is False:
+        if ac3_optimized(self.grid) is False:
             return Sudoku.SolutionState.NoSolution
 
-        # TODO:
-        #  - heuristics (MRV, LCV)
-        for i in range(9):
-            for j in range(9):
-                cell = (i, j)
-                cell_values = self.grid[cell]
-                if cell_values.bit_count() == 1:
-                    continue
+        # Find the cell with the fewest possibilities (MRV heuristic)
+        count_bits = np.vectorize(lambda x: x.bit_count() if x.bit_count() != 1 else 10)
+        cell = np.argmin(count_bits(self.grid))
+        cell = (cell // 9, cell % 9)
 
-                solutions = []
+        if self.grid[cell].bit_count() == 1:
+            # All cells have exactly one value
+            return Sudoku.SolutionState.UniqueSolution
 
-                # Try each possibility
-                for num in [k + 1 for k in range(9) if self.grid[cell] & (1 << k)]:
-                    new_grid = self.grid.copy()
-                    new_grid[cell] = 1 << (num - 1)
-                    new_sudoku = Sudoku(self.initial_grid, new_grid)
-                    if new_sudoku.solve() != Sudoku.SolutionState.NoSolution:
-                        solutions.append(new_sudoku.grid)
+        solutions = []
 
-                match len(solutions):
-                    case 0:
-                        return Sudoku.SolutionState.NoSolution
-                    case 1:
-                        self.grid = solutions[0]
-                        return Sudoku.SolutionState.UniqueSolution
-                    case _:
-                        # merge the solutions
-                        for x in [(i, j) for i in range(9) for j in range(9)]:
-                            self.grid[x] = 0
-                            for solution in solutions:
-                                self.grid[x] |= solution[x]
-                        return Sudoku.SolutionState.MultipleSolutions
+        # Try each possibility
+        for num in [k + 1 for k in range(9) if self.grid[cell] & (1 << k)]:
+            new_grid = self.grid.copy()
+            new_grid[cell] = 1 << (num - 1)
+            new_sudoku = Sudoku(self.initial_grid, new_grid)
+            if new_sudoku.solve() != Sudoku.SolutionState.NoSolution:
+                solutions.append(new_sudoku.grid)
 
-        return Sudoku.SolutionState.UniqueSolution
+        match len(solutions):
+            case 0:
+                return Sudoku.SolutionState.NoSolution
+            case 1:
+                self.grid = solutions[0]
+                return Sudoku.SolutionState.UniqueSolution
+            case _:
+                # merge the solutions
+                for x in [(i, j) for i in range(9) for j in range(9)]:
+                    self.grid[x] = 0
+                    for solution in solutions:
+                        self.grid[x] |= solution[x]
+                return Sudoku.SolutionState.MultipleSolutions
 
     @staticmethod
     def ac3(domains: np.ndarray[(9, 9), set[int]]) -> bool:
@@ -104,13 +103,6 @@ class Sudoku:
         :param domains: Domains of the variables
         :return: True if the domains are arc consistent, False otherwise
         """
-
-        def r2(x: Tuple[int, int], y: Tuple[int, int]):
-            return (x != y) and (
-                    (x[0] == y[0]) or  # same row
-                    (x[1] == y[1]) or  # same column
-                    (x[0] // 3 == y[0] // 3 and x[1] // 3 == y[1] // 3)  # same box
-            )
 
         def arc_reduce(x, y):
             """
@@ -128,6 +120,13 @@ class Sudoku:
 
             return False
 
+        def r2(x: Tuple[int, int], y: Tuple[int, int]):
+            return (x != y) and (
+                    (x[0] == y[0]) or  # same row
+                    (x[1] == y[1]) or  # same column
+                    (x[0] // 3 == y[0] // 3 and x[1] // 3 == y[1] // 3)  # same box
+            )
+
         X = [(i, j) for i in range(9) for j in range(9)]
         worklist = [(x, y) for x in X for y in X if r2(x, y)]
 
@@ -139,6 +138,19 @@ class Sudoku:
                 for z in X:
                     if z != y and r2(z, x):
                         worklist.append((z, x))
+        return True
+
+    def minimum_clues(self):
+        """
+        Checks for early termination conditions:
+            - Must have >= 17 clues
+            - Must have 8, 9 distinct digits
+        :return: True if the puzzle satisfies the conditions
+        """
+        if len(np.unique(self.initial_grid[self.initial_grid != 0])) < 8:
+            return False
+        if np.count_nonzero(self.initial_grid) < 17:
+            return False
         return True
 
 
@@ -155,6 +167,50 @@ def from_string(string: str) -> Sudoku:
     return Sudoku(board)
 
 
+def calc_worklist():
+    def r2(x, y):
+        return (x != y) and (
+                (x[0] == y[0]) or  # same row
+                (x[1] == y[1]) or  # same column
+                (x[0] // 3 == y[0] // 3 and x[1] // 3 == y[1] // 3)  # same box
+        )
+
+    X = [(i, j) for i in range(9) for j in range(9)]
+    return [(x, y) for x in X for y in X if r2(x, y)]
+
+
+# precompute worklist and neighbors for faster execution
+WORKLIST = calc_worklist()
+NEIGHBORS = [
+    [
+        *[(i, k) for k in range(9) if k != j],  # Row
+        *[(k, j) for k in range(9) if k != i],  # Column
+        *[(i // 3 * 3 + di, j // 3 * 3 + dj)
+          for di in range(3) for dj in range(3)
+          if di != i % 3 and dj != j % 3]  # Box (w/o same row/column)
+    ] for i in range(9) for j in range(9)
+]
+
+
+def ac3_optimized(domains: np.ndarray[(9, 9), set[int]]) -> bool:
+    worklist = WORKLIST.copy()  # precomputed worklist with all constraints
+
+    while worklist:
+        x, y = worklist.pop()
+        y_val = domains[y]
+        if (y_val & (y_val - 1)) == 0 and domains[x] & y_val:
+            domains[x] &= ~y_val
+
+            if not domains[x]:
+                return False
+
+            for neighbor in NEIGHBORS[x[0] * 9 + x[1]]:
+                if neighbor != y:
+                    worklist.append((neighbor, x))
+
+    return True
+
+
 def main():
     # given in assignment pdf:
     sudoku = Sudoku([
@@ -169,24 +225,32 @@ def main():
         [0, 2, 4, 0, 3, 5, 0, 0, 0]
     ])
 
-    # very slow cases:
-    sudoku = from_string("4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......")
+    # slow cases:
+    # sudoku = from_string("4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......")
+    # https://en.wikipedia.org/wiki/File:Sudoku_puzzle_hard_for_brute_force.svg
     sudoku = from_string("..............3.85..1.2.......5.7.....4...1...9.......5......73..2.1........4...9")
+    # Not enough unique values:
     # sudoku = from_string(".....5.8....6.1.43..........1.5........1.6...3.......553.....61........4.........")
     # sudoku = from_string("9..8...........5............2..1...3.1.....6....4...7.7.86.........3.1..4.....2..")
 
     print(f"{sudoku:color}")
     print("-" * 25)
+    start = time.time()
 
-    match sudoku.solve():
-        case Sudoku.SolutionState.NoSolution:
-            print("No solution")
-        case Sudoku.SolutionState.UniqueSolution:
-            print("Unique solution")
-            print(f"{sudoku:color}")
-        case Sudoku.SolutionState.MultipleSolutions:
-            print("Multiple solutions")
-            print(f'{sudoku:color}')
+    if not sudoku.minimum_clues():
+        print("Not enough clues (>= 17, > 7 distinct digits)")
+    else:
+        match sudoku.solve():
+            case Sudoku.SolutionState.NoSolution:
+                print("No solution")
+            case Sudoku.SolutionState.UniqueSolution:
+                print("Unique solution")
+                print(f"{sudoku:color}")
+            case Sudoku.SolutionState.MultipleSolutions:
+                print("Multiple solutions")
+                print(f'{sudoku:color}')
+
+    print(f"Solution found in {time.time() - start:.2f}s")
 
 
 if __name__ == '__main__':
